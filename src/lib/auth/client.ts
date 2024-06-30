dbf1b43aaa2c6a7861cdbb08a3256b0873533f5e
Adama Jarju <jarjuadama101@gmail.com>
2024-06-30 09:57:10 +0000

    made some changes and updated the pnpm-lock.yaml file

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -11,9 +11,9 @@ function generateToken(): string {
 const user = {
   id: 'USR-000',
   avatar: '/assets/avatar.png',
-  firstName: 'Sofia',
-  lastName: 'Rivers',
-  email: 'sofia@devias.io',
+  firstName: 'Adama',
+  lastName: 'Jarju',
+  email: 'adama.jarju@meltwater.org',
 } satisfies User;
 
 export interface SignUpParams {
@@ -57,7 +57,7 @@ class AuthClient {
     // Make API request
 
     // We do not handle the API, so we'll check if the credentials match with the hardcoded ones.
-    if (email !== 'sofia@devias.io' || password !== 'Secret1') {
+    if (email !== '' || password !== '') {
       return { error: 'Invalid credentials' };
     }
 
